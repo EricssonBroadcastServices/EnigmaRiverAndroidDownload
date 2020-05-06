6956c9cb228872c5c88df274b06b58087cf84b6f
empjenk <empjenk@ericsson.com>
2020-05-06 12:17:25 +0000

    r3.0.4-BETA-4

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Initial commit
+# Download module
+
+The Enigma River download extension library, version r3.0.4-BETA-4.
---
+++ src/main/java/com/redbeemedia/enigma/download/Placeholder.java
@@ -0,0 +1,5 @@
+package com.redbeemedia.enigma.download;
+
+public class Placeholder {
+    public static final String DOWNLOAD = "DOWNLOAD";
+}
